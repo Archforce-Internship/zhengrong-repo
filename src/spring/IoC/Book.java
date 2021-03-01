2305abbae78c497cc016d8a9ee8b39985eff0a93
misaka517 <136317512@qq.com>
2021-03-01 20:48:17 +0800

    空值和特殊符号

--- src/spring/IoC/Book.java
+++ src/spring/IoC/Book.java
@@ -8,7 +8,11 @@ public class Book {
 
 
     public String bName;
+    public String value1;
 
+    public void setValue1(String value1) {
+        this.value1 = value1;
+    }
 
     public void setbName(String bName) {
         this.bName = bName;
@@ -17,7 +21,7 @@ public class Book {
 //set加配置文件注入属性
 
     public void scan(){
-        System.out.println(bName);
+        System.out.println(bName + "" + value1);
     }
 
 }
