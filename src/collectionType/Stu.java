1841649273af76ca0d46f399913219cc44c3ea33
misaka517 <136317512@qq.com>
2021-03-02 14:36:01 +0800

    配置文件注入对象数组

--- src/collectionType/Stu.java
+++ src/collectionType/Stu.java
@@ -7,31 +7,25 @@ import java.util.*;
  * @date 2021-03-02 - 13:53
  */
 public class Stu {
-    private String[] courses;
-
-    private List<String> list;
+    private List<courses> coursesList;
     private Map<String,String> map;
     private Set<String> set;
     public void setSet(Set<String> set) {
         this.set = set;
     }
 
-    public void setList(List<String> list) {
-        this.list = list;
-    }
-
     public void setMap(Map<String, String> map) {
         this.map = map;
     }
 
-    public void setCourses(String[] courses) {
-        this.courses = courses;
+    public void setCoursesList(List<courses> coursesList) {
+        this.coursesList = coursesList;
     }
 
     public void test(){
-        System.out.println(Arrays.toString(courses));
-        System.out.println(list);
-        System.out.println(map);
-        System.out.println(set);
+//        System.out.println(courses);
+//        System.out.println(map);
+//        System.out.println(set);
+        System.out.println(coursesList);
     }
 }
---
+++ src/collectionType/courses.java
@@ -0,0 +1,20 @@
+package collectionType;
+
+/**
+ * @author 517
+ * @date 2021-03-02 - 14:21
+ */
+public class courses {
+    public String coursesName;
+
+    public void setCoursesName(String coursesName) {
+        this.coursesName = coursesName;
+    }
+
+    @Override
+    public String toString() {
+        return "courses{" +
+                "coursesName='" + coursesName + '\'' +
+                '}';
+    }
+}
